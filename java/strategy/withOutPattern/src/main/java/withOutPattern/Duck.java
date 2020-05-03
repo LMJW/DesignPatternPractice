dd4b962f7fd154c970b85496b08baed05b56c086
LMJW <heysuperming@gmail.com>
2020-05-03 16:54:38 +1000

    Modify the parent class will change all the behaviors of the subclasses

--- java/strategy/withOutPattern/src/main/java/withOutPattern/Duck.java
+++ java/strategy/withOutPattern/src/main/java/withOutPattern/Duck.java
@@ -12,4 +12,14 @@ public class Duck {
   String display() {
     return "a pretty duck";
   }
+
+  /// the problem with modifying the base class is that all the subclasses will
+  /// get additional property which might change the behavior of the children
+  /// classes. Therefore, this is not a good pattern for some use cases.
+
+  /// Maybe only use this pattern if we actually want to change all the
+  /// behaviors of the subclasses.
+  String fly() {
+    return "i can fly";
+  }
 }
---
+++ java/strategy/withOutPattern/src/main/java/withOutPattern/WoodenDuck.java
@@ -0,0 +1,8 @@
+package withOutPattern;
+
+public class WoodenDuck extends Duck {
+  String quack() {
+    // wooden duck can not quark
+    return "";
+  }
+}
