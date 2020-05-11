60047dd497ac883cdde95297b01e15cd4391465b
LMJW <heysuperming@gmail.com>
2020-05-11 17:47:05 +1000

    Initialize the instance using static initializer.

--- java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
+++ java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
@@ -7,27 +7,20 @@ package chocolateFactory;
 public class ChocolateBoiler {
   private boolean empty;
   private boolean boiled;
-  private static ChocolateBoiler boiler;
+  /**
+   * The second implementation is to create the instance with static
+   * initializer. In this way, the code is gurantee to thread safe.
+   */
+  private static ChocolateBoiler boiler = new ChocolateBoiler();
 
   private ChocolateBoiler() {
     empty = true;
     boiled = false;
   }
 
-  /**
-   * The first method is to use the `synchronized` keyword. This keyword will
-   * force every thread to wait its turn before it can enter the method. But,
-   * you need to keep in mind that the `synchronized` method can decrease
-   * performance by a factor of `100`(rule of thumb). 
-   *
-   * @Note: the 100 times slower is taken from a old java book, which might not
-   * true now. 
-   */
+ 
 
   public static synchronized ChocolateBoiler getInstance() {
-    if (boiler == null) {
-      boiler = new ChocolateBoiler();
-    }
     return boiler;
   }
 
