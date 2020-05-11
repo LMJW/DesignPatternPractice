447e14af9bfb0f517ab3e30ca751fc3c2f7ac7da
LMJW <heysuperming@gmail.com>
2020-05-11 16:50:34 +1000

    Limitation of the simple implementation

--- java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
+++ java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
@@ -1,5 +1,9 @@
 package chocolateFactory;
-
+/**
+ * The current singleton pattern implementation did not take into account of the
+ * multi-threading environment. To enable the current pattern to work for the
+ * multi-threading environment, there are several ways to do this
+ */
 public class ChocolateBoiler {
   private boolean empty;
   private boolean boiled;
