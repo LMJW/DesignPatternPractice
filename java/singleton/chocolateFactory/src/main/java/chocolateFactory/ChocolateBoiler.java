ebe0cc2a76de7235ebed051d5853885d446f39dc
LMJW <heysuperming@gmail.com>
2020-05-11 17:00:16 +1000

    Use `synchronized` keyword to make the current implementation thread-safe

--- java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
+++ java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
@@ -14,7 +14,17 @@ public class ChocolateBoiler {
     boiled = false;
   }
 
-  public static ChocolateBoiler getInstance() {
+  /**
+   * The first method is to use the `synchronized` keyword. This keyword will
+   * force every thread to wait its turn before it can enter the method. But,
+   * you need to keep in mind that the `synchronized` method can decrease
+   * performance by a factor of `100`(rule of thumb). 
+   *
+   * @Note: the 100 times slower is taken from a old java book, which might not
+   * true now. 
+   */
+
+  public static synchronized ChocolateBoiler getInstance() {
     if (boiler == null) {
       boiler = new ChocolateBoiler();
     }
