4218b0acaf60f47685028017103eb74fee8b1650
LMJW <heysuperming@gmail.com>
2020-05-11 18:00:58 +1000

    Use `double checked locking` to resolve the thread-safe problem.

--- java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
+++ java/singleton/chocolateFactory/src/main/java/chocolateFactory/ChocolateBoiler.java
@@ -1,26 +1,31 @@
 package chocolateFactory;
 /**
- * The current singleton pattern implementation did not take into account of the
- * multi-threading environment. To enable the current pattern to work for the
- * multi-threading environment, there are several ways to do this
+ * The current singleton pattern implementation did not take into account of the multi-threading
+ * environment. To enable the current pattern to work for the multi-threading environment, there are
+ * several ways to do this
  */
 public class ChocolateBoiler {
   private boolean empty;
   private boolean boiled;
-  /**
-   * The second implementation is to create the instance with static
-   * initializer. In this way, the code is gurantee to thread safe.
-   */
-  private static ChocolateBoiler boiler = new ChocolateBoiler();
+  private static volatile ChocolateBoiler boiler;
 
   private ChocolateBoiler() {
     empty = true;
     boiled = false;
   }
 
- 
-
-  public static synchronized ChocolateBoiler getInstance() {
+  /**
+   * This is a method called 'double checked locking', this can be used to reduce the use of
+   * synchornization in getInstance(). This can reduce the overhead.
+   */
+  public static ChocolateBoiler getInstance() {
+    if (boiler == null) {
+      synchronized (ChocolateBoiler.class) {
+        if (boiler == null) {
+          boiler = new ChocolateBoiler();
+        }
+      }
+    }
     return boiler;
   }
 
